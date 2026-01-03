80d3c3f9275db5e3f1d94cb9e7a5fac7633edc3c
sphinx <okhaled32111@gmail.com>
2026-01-03 11:50:06 +0200

    update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,13 +4,16 @@ set -euo pipefail
 echo "Checking for updates..."
 
 
+
+LOCAL=$(git rev-parse HEAD)
 LOCAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo "Local branch: $LOCAL_BRANCH"
 
-REMOTE_BRANCH=$(git rev-parse --abbrev-ref origin/$LOCAL_BRANCH)
-echo "Remote branch: $REMOTE_BRANCH"
 
-if [ "$LOCAL_BRANCH" != "$REMOTE_BRANCH" ]; then
+
+REMOTE=$(git rev-parse origin/$LOCAL_BRANCH)
+
+
+if [ "$LOCAL" != "$REMOTE" ]; then
     echo "Local branch is not equal to remote branch. Please update your local branch."
     exit 1
 fi
