7ca3a36027b51151dd590b633b063c482bd0c86c
sphinx <okhaled32111@gmail.com>
2026-01-03 17:11:15 +0200

    update

--- infra/jenkins/scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Deploying... here we gooooo"
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
-
-docker build -t internal-backend $SCRIPT_DIR/services/backend
