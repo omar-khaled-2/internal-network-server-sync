c027a722574a10d1edd0aaf146cb1103f680162e
sphinx <okhaled32111@gmail.com>
2026-01-03 11:57:03 +0200

    update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -12,6 +12,7 @@ LOCAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 REMOTE=$(git rev-parse origin/$LOCAL_BRANCH)
 
 if [ "$LOCAL" != "$REMOTE" ]; then
-    echo "Local branch is not equal to remote branch. Please update your local branch."
-    exit 1
+    git pull
+    echo "Updated successfully"
+    ./deploy.sh
 fi
